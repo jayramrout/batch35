c5e32500c27234eb7e53d814179022150329040e
Jayram Rout <jrout@healthedge.com>
2021-03-17 21:56:25 -0400

    Updaets

---
+++ dvdapp/src/main/java/jrout/tutorial/batch35/dvdapp/DvdappApplication.java
@@ -0,0 +1,13 @@
+package jrout.tutorial.batch35.dvdapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DvdappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DvdappApplication.class, args);
+    }
+
+}
---
+++ dvdapp/src/test/java/jrout/tutorial/batch35/dvdapp/DvdappApplicationTests.java
@@ -0,0 +1,13 @@
+package jrout.tutorial.batch35.dvdapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DvdappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
